c9a17e95f1b0d1d3d3bc51b2160a8f6482fda7a1
Oliver Nordbjerg <hi@notbjerg.me>
2018-03-14 21:15:44 +0100

    messenger: Add docblock to DevMessage provider

--- packages/aragon-messenger/src/providers/DevMessage.js
+++ packages/aragon-messenger/src/providers/DevMessage.js
@@ -1,5 +1,32 @@
 import Provider from './Provider'
 
+/**
+ * A development message provider that communicates using an RxJS subject.
+ *
+ * Example:
+ *
+ * ```js
+ * // Message bus used to pass messages between apps and the wrapper
+ * const bus = new Subject()
+ *
+ * // Set up app
+ * const app = new App(
+ *   new Messenger(new DevMessage('app', 'wrapper', bus))
+ * )
+ *
+ * // Run app
+ * wrapper.runApp(
+ *   new DevMessage('wrapper', 'app', bus),
+ *   '0xbitconnect'
+ * )
+ * ```
+ *
+ * @param {string} id The ID of this specific entity (e.g. "wrapper")
+ * @param {string} target The ID of the target entity to communicate with (e.g. "app")
+ * @param {Subject} bus A shared RxJS subject used to communicate between different entities
+ * @class DevMessage
+ * @extends {Provider}
+ */
 export default class DevMessage extends Provider {
   constructor (id, target, bus) {
     super()
