26818378f5352e2d93f07e9c5612b495dbc93a63
Oliver Nordbjerg <hi@notbjerg.me>
2018-03-15 17:56:04 +0100

    wrapper: Fixes to `Proxy#events`

--- packages/aragon-wrapper/src/core/proxy/index.js
+++ packages/aragon-wrapper/src/core/proxy/index.js
@@ -27,8 +27,38 @@ export default class Proxy {
   }
 
   // TODO: Make this a hot observable?
-  async events (eventNames) {
-    const streamingPredicate = await this.streamingPredicate()
+  events (eventNames) {
+    const streamingPredicate = Observable.fromPromise(
+      this.streamingPredicate()
+    )
+
+    const filterOldEvents = (streamingPredicate) => (event) => {
+      const isFreshCursor = streamingPredicate.transactionIndex === null || streamingPredicate.logIndex === null
+
+      // If this is a "fresh cursor", i.e. we haven't cached any cursor yet,
+      // we will allow processing of everything
+      if (isFreshCursor) return true
+
+      const blockMatchesCursor = event.blockNumber === streamingPredicate.blockNumber
+      if (blockMatchesCursor) {
+        const transactionMatchesCursor = event.transactionIndex === streamingPredicate.transactionIndex
+
+        // If we're at the transaction our cursor is at, then
+        // we check if the log we're processing is after than the
+        // one we have already processed
+        if (transactionMatchesCursor) {
+          return event.logIndex > streamingPredicate.logIndex
+        }
+
+        // We're not at the transaction we've processed, so we will only
+        // allow events to be emitted from later transactions
+        return event.transactionIndex > streamingPredicate.transactionIndex
+      }
+
+      // We're not at the cursor block, so we will only process events
+      // from later blocks
+      return event.blockNumber > streamingPredicate.blockNumber
+    }
 
     // Get all events
     if (!eventNames) {
@@ -44,50 +74,27 @@ export default class Proxy {
     let eventSource
     if (eventNames.length === 1) {
       // Get a specific event
-      eventSource = Observable.fromEvent(
-        this.contract.events[eventNames[0]]({
-          fromBlock: streamingPredicate.fromBlock
-        }), 'data'
+      eventSource = streamingPredicate.switchMap((streamingPredicate) =>
+        Observable.fromEvent(
+          this.contract.events[eventNames[0]]({
+            fromBlock: streamingPredicate.blockNumber
+          }), 'data'
+        ).filter(filterOldEvents(streamingPredicate))
       )
     } else {
       // Get multiple events
-      eventSource = Observable.fromEvent(
-        this.contract.events.allEvents({
-          fromBlock: streamingPredicate.fromBlock
-        }), 'data'
-      ).filter(
-        (event) => eventNames.includes(event.event)
+      eventSource = streamingPredicate.switchMap((streamingPredicate) =>
+        Observable.fromEvent(
+          this.contract.events.allEvents({
+            fromBlock: streamingPredicate.blockNumber
+          }), 'data'
+        ).filter(
+          (event) => eventNames.includes(event.event)
+        ).filter(filterOldEvents(streamingPredicate))
       )
     }
 
     return eventSource
-      .filter((event) => {
-        const isFreshCursor = streamingPredicate.transactionIndex === null || streamingPredicate.logIndex === null
-
-        // If this is a "fresh cursor", i.e. we haven't cached any cursor yet,
-        // we will allow processing of everything
-        if (isFreshCursor) return true
-
-        const blockMatchesCursor = event.blockNumber === streamingPredicate.blockNumber
-        if (blockMatchesCursor) {
-          const transactionMatchesCursor = event.transactionIndex === streamingPredicate.transactionIndex
-
-          // If we're at the transaction our cursor is at, then
-          // we check if the log we're processing is after than the
-          // one we have already processed
-          if (transactionMatchesCursor) {
-            return event.logIndex > streamingPredicate.logIndex
-          }
-
-          // We're not at the transaction we've processed, so we will only
-          // allow events to be emitted from later transactions
-          return event.transactionIndex > streamingPredicate.transactionIndex
-        }
-
-        // We're not at the cursor block, so we will only process events
-        // from later blocks
-        return event.blockNumber > streamingPredicate.blockNumber
-      })
   }
 
   call (method, ...params) {
