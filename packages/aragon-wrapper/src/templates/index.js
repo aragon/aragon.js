22e5bc523ceb003cf487829e95b3402ec80f5c83
Oliver Nordbjerg <hi@notbjerg.me>
2018-03-28 16:34:43 +0200

    wrapper: Set templates gas to 6.7m

--- packages/aragon-wrapper/src/templates/index.js
+++ packages/aragon-wrapper/src/templates/index.js
@@ -37,7 +37,7 @@ const Templates = (web3, apm, from) => {
     const call = template.methods.newToken(name, name)
     const receipt = await call.send({
       from,
-      gas: 6500000,
+      gas: 6700000,
       gasPrice: minGasPrice
     })
     return receipt.events.DeployToken.returnValues
@@ -47,7 +47,7 @@ const Templates = (web3, apm, from) => {
     const call = template.methods.newInstance(name, ...params)
     const receipt = await call.send({
       from,
-      gas: 6500000,
+      gas: 6700000,
       gasPrice: minGasPrice
     })
     return receipt.events.DeployInstance.returnValues
