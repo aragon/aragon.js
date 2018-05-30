a542150747e63b81845015ce6f9d723ec085c607
ßingen <bingentxu@gmail.com>
2018-05-30 19:24:53 +0200

    Fix issue with cloudflare non-checksummed addresses
    Adding `addressesEqual` comparison everywhere to ensure that checks
    pass even if only one of the compared addresses are not checksummed.

--- packages/aragon-wrapper/src/index.js
+++ packages/aragon-wrapper/src/index.js
@@ -414,7 +414,7 @@ export default class Aragon {
     // NOTE: we **CANNOT** use this.apps here, as it'll trigger an endless spiral of infinite streams
     const proxy = this.appsWithoutIdentifiers
       .map((apps) => apps.find(
-        (app) => app.proxyAddress === proxyAddress)
+        (app) => addressesEqual(app.proxyAddress, proxyAddress))
       )
       .map(
         (app) => makeProxyFromABI(app.proxyAddress, app.abi, this.web3)
@@ -610,7 +610,7 @@ export default class Aragon {
 
     const permissions = await this.permissions.take(1).toPromise()
     const app = await this.apps.map(
-      (apps) => apps.find((app) => app.proxyAddress === destination)
+      (apps) => apps.find((app) => addressesEqual(app.proxyAddress, destination))
     ).take(1).toPromise()
     let forwarders = await this.forwarders.take(1).toPromise().then(
       (forwarders) => forwarders.map(
