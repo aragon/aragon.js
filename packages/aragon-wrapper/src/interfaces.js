53d5d09cc4f442bef0355f8c14016edd3eead828
Jorge Izquierdo <jorge@izqui.me>
2018-09-06 18:47:18 +0200

    Add Kernel and EVMScriptRegistry artifacts

--- packages/aragon-wrapper/src/interfaces.js
+++ packages/aragon-wrapper/src/interfaces.js
@@ -3,21 +3,25 @@ import abiAragonACL from '../abi/aragon/ACL.json'
 import abiAragonAppProxy from '../abi/aragon/AppProxy.json'
 import abiAragonForwarder from '../abi/aragon/Forwarder.json'
 import abiAragonKernel from '../abi/aragon/Kernel.json'
+import abiAragonEVMScriptRegistry from '../abi/aragon/EVMScriptRegistry.json'
 
 // Artifacts
 import artifactsAragonACL from '../artifacts/aragon/ACL.json'
+import artifactsAragonKernel from '../artifacts/aragon/Kernel.json'
+import artifactsAragonEVMScriptRegistry from '../artifacts/aragon/EVMScriptRegistry.json'
 
 const ABIS = {
   'aragon/ACL': abiAragonACL,
   'aragon/AppProxy': abiAragonAppProxy,
   'aragon/Forwarder': abiAragonForwarder,
   'aragon/Kernel': abiAragonKernel,
-  'aragon/EVM Script Registry': {} // TODO
+  'aragon/EVM Script Registry': abiAragonEVMScriptRegistry
 }
 
 const ARTIFACTS = {
   'aragon/ACL': artifactsAragonACL,
-  'aragon/EVM Script Registry': {} // TODO
+  'aragon/Kernel': artifactsAragonKernel,
+  'aragon/EVM Script Registry': artifactsAragonEVMScriptRegistry
 }
 
 export const getAbi = name => ABIS[name] || null
