1e75d1349adfc81dd4f8ff858c593f53404df4cb
Brett Sun <qisheng.brett.sun@gmail.com>
2018-09-06 15:44:24 +0200

    Add old 0.5 Rinkeby DAO app IDs that were generated incorrectly

--- packages/aragon-wrapper/src/core/aragonOS/index.js
+++ packages/aragon-wrapper/src/core/aragonOS/index.js
@@ -1,13 +1,22 @@
 import { hash as namehash } from 'eth-ens-namehash'
+import Web3 from 'web3'
 import { getAbi, getArtifact } from '../../interfaces'
 
-const aragonpmName = appName => namehash(`${appName}.aragonpm.eth`)
+// TODO: Remove this when 0.5 Rinkeby DAOs are deprecated
+const oldWrongAppId = appName => Web3.utils.soliditySha3(`${appName}.aragonpm.eth`)
+
+const aragonpmAppId = appName => namehash(`${appName}.aragonpm.eth`)
 
 const APP_MAPPINGS = {
-  [aragonpmName('acl')]: 'ACL',
+  [aragonpmAppId('acl')]: 'ACL',
+  [aragonpmAppId('evmreg')]: 'EVMScriptRegistry',
+
+  // TODO: Remove this when 0.5 Rinkeby DAOs are deprecated
+  [oldWrongAppId('acl')]: 'ACL',
+  [oldWrongAppId('evmreg')]: 'EVMScriptRegistry'
 }
 
-function getAragonOSAppInfo(appId) {
+function getAragonOSAppInfo (appId) {
   const appName = APP_MAPPINGS[appId]
 
   if (!appName) {
