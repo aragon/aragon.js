afc123fe6796a567e2a8907eba90286b4dc00681
Brett Sun <qisheng.brett.sun@gmail.com>
2018-10-12 11:39:33 +0200

    tests: fix whitespace

--- packages/aragon-wrapper/src/utils/index.test.js
+++ packages/aragon-wrapper/src/utils/index.test.js
@@ -3,23 +3,23 @@ import sinon from 'sinon'
 import * as utils from './index'
 
 test.afterEach.always(() => {
-    sinon.restore()
+  sinon.restore()
 })
 
 test('should enhance an object to lookup eth addresses easier', async (t) => {
-    // arrange
-    const bobAddress = '0x0000000000000000000000000000000000000aBc'
-    const bobPermissions = ['read', 'write']
-    // act
-    const permissions = utils.makeAddressMapProxy({})
-    permissions[bobAddress] = bobPermissions
-    // assert
-    t.is(permissions['0x0000000000000000000000000000000000000ABC'], bobPermissions)
-    t.is(permissions['0x0000000000000000000000000000000000000abc'], bobPermissions)
-    t.is(permissions['0x0000000000000000000000000000000000000aBc'], bobPermissions)
-    // addresses with invalid checksums 
-    // (the checksum is checked if the address has both upper and lowercase letters)
-    t.is(permissions['0x0000000000000000000000000000000000000aBC'], undefined)
-    t.is(permissions['0x0000000000000000000000000000000000000abC'], undefined)
-    t.is(permissions['0x0000000000000000000000000000000000000ABc'], undefined)
+  // arrange
+  const bobAddress = '0x0000000000000000000000000000000000000aBc'
+  const bobPermissions = ['read', 'write']
+  // act
+  const permissions = utils.makeAddressMapProxy({})
+  permissions[bobAddress] = bobPermissions
+  // assert
+  t.is(permissions['0x0000000000000000000000000000000000000ABC'], bobPermissions)
+  t.is(permissions['0x0000000000000000000000000000000000000abc'], bobPermissions)
+  t.is(permissions['0x0000000000000000000000000000000000000aBc'], bobPermissions)
+  // addresses with invalid checksums
+  // (the checksum is checked if the address has both upper and lowercase letters)
+  t.is(permissions['0x0000000000000000000000000000000000000aBC'], undefined)
+  t.is(permissions['0x0000000000000000000000000000000000000abC'], undefined)
+  t.is(permissions['0x0000000000000000000000000000000000000ABc'], undefined)
 })
