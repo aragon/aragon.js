f6d8a56b30221a1db176b735e6a56fa493c6b391
mulili <mulili.nzuki@gmail.com>
2018-10-31 18:42:18 +0300

    init signing data code

--- packages/aragon-client/src/index.js
+++ packages/aragon-client/src/index.js
@@ -276,7 +276,6 @@ export class AppProxy {
    /**
    * Allow apps to sign arbitrary data via a RPC call
    *
-   * @param  {string} dataToSign
    * @return {Observable} An observable that emits the signature of the signed data
    */
   signData () {
