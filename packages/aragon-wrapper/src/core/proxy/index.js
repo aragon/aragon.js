b43fd0a700a44e4a341d4129c014c0c78bfda51d
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-09 20:54:30 +0200

    Wrapper: revert removal of async in proxy.call()

--- packages/aragon-wrapper/src/core/proxy/index.js
+++ packages/aragon-wrapper/src/core/proxy/index.js
@@ -45,7 +45,7 @@ export default class Proxy {
     return eventSource
   }
 
-  call (method, ...params) {
+  async call (method, ...params) {
     if (!this.contract.methods[method]) {
       throw new Error(`No method named ${method} on ${this.address}`)
     }
