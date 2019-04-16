1bb8013dfe286822e9ff8261b7d67daae67426d4
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-16 08:59:27 -0400

    Update packages/aragon-wrapper/src/rpc/handlers/sign-message.js
    Co-Authored-By: Schwartz10 <Schwartz10@users.noreply.github.com>

--- packages/aragon-wrapper/src/rpc/handlers/sign-message.js
+++ packages/aragon-wrapper/src/rpc/handlers/sign-message.js
@@ -1,7 +1,7 @@
 export default function (request, proxy, wrapper) {
   const message = request.params[0]
   if (typeof message !== 'string') {
-    throw new Error('typeof message to sign must be a string')
+    throw new Error('Message to sign must be a string')
   }
   return wrapper.signMessage(message, proxy.address)
 }
