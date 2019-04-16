84d5dc276235326dc452e9d661f27ad5a2d27cfe
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-04-16 07:43:24 -0400

    enforce typeof message to sign is string

--- packages/aragon-wrapper/src/rpc/handlers/sign-message.js
+++ packages/aragon-wrapper/src/rpc/handlers/sign-message.js
@@ -1,4 +1,7 @@
 export default function (request, proxy, wrapper) {
   const message = request.params[0]
+  if (typeof message !== 'string') {
+    throw new Error('typeof message to sign must be a string')
+  }
   return wrapper.signMessage(message, proxy.address)
 }
