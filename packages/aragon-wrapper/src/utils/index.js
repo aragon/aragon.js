52b2a325a3e9687488f782d7df0e07b1e55eb28d
Brett Sun <qisheng.brett.sun@gmail.com>
2019-04-26 19:00:23 +0200

    docs: document findMethodOnAppFromData()

--- packages/aragon-wrapper/src/utils/index.js
+++ packages/aragon-wrapper/src/utils/index.js
@@ -49,6 +49,14 @@ export function makeProxyFromABI (address, abi, web3, initializationBlock) {
   return new ContractProxy(address, abi, web3, initializationBlock)
 }
 
+/**
+ * Find the method descriptor corresponding to the data component of a
+ * transaction sent to `app`.
+ *
+ * @param  {Object} data Data component of a transaction to app
+ * @param  {Object} app App artifact
+ * @return {Object} Method with radspec notice and function signature
+ */
 export function findMethodOnAppFromData (data, app) {
   if (app && app.functions) {
     // Find the method
