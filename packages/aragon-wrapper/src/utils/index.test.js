9cb51e6532cd97997cfce4f058e50c6a6c5a3a10
Daniel Norman <daniel@2color.me>
2019-05-14 13:58:31 +0800

    Add tests with mixed cases for addressMapProxy

--- packages/aragon-wrapper/src/utils/index.test.js
+++ packages/aragon-wrapper/src/utils/index.test.js
@@ -24,17 +24,30 @@ test('should enhance an object to lookup eth addresses easier', async (t) => {
   t.is(permissions['0x0000000000000000000000000000000000000ABc'], undefined)
 })
 
-test('should initialise the proxy with its keys in lowercase', async (t) => {
+test('should allow the proxy to be initialized with an object containing any cased keys', async (t) => {
   // arrange
   const dianeAddress = '0x0000000000000000000000000000000000000ABC'
+  const annieAddress = '0x0000000000000000000000000000000000000cde'
+  const rainiAddress = '0x0000000000000000000000000000000000000eED'
   const dianePermissions = ['read', 'write']
+  const anniePermissions = ['read', 'sing']
+  const rainiPermissions = ['dance', 'modify']
   // act
   const permissions = utils.makeAddressMapProxy({
-    [dianeAddress]: dianePermissions
+    [dianeAddress]: dianePermissions,
+    [annieAddress]: anniePermissions,
+    [rainiAddress]: rainiPermissions
   })
-  // permissions[bobAddress] = bobPermissions
+
   // assert
   t.is(permissions['0x0000000000000000000000000000000000000ABC'], dianePermissions)
   t.is(permissions['0x0000000000000000000000000000000000000abc'], dianePermissions)
   t.is(permissions['0x0000000000000000000000000000000000000aBc'], dianePermissions)
+
+  t.is(permissions['0x0000000000000000000000000000000000000CDE'], anniePermissions)
+  t.is(permissions['0x0000000000000000000000000000000000000cde'], anniePermissions)
+
+  t.is(permissions['0x0000000000000000000000000000000000000EED'], rainiPermissions)
+  t.is(permissions['0x0000000000000000000000000000000000000eed'], rainiPermissions)
+  t.is(permissions['0x0000000000000000000000000000000000000EeD'], rainiPermissions)
 })
