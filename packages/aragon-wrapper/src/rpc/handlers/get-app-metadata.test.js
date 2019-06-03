eaa0cbffd0b735585142fab2c06e534f53d2221c
rkzel <rkz@hyperlogos.net>
2019-06-03 13:47:05 +0200

    metadata get+register handlers

---
+++ packages/aragon-wrapper/src/rpc/handlers/get-app-metadata.test.js
@@ -0,0 +1,37 @@
+import test from 'ava'
+import getAppMetadata from './get-app-metadata'
+import { from } from 'rxjs'
+
+test.only('should receive and filter the app metadata registry', async (t) => {
+  t.plan(1)
+
+  const getAppMetadataObservable = from([
+    [{
+      from: '0x73a',
+      to: ['0xdeadcafe'],
+      dataId: 'u1',
+      cid: 'Qmrandomhash'
+    },
+    {
+      from: '0xfed',
+      to: ['0xcafe','0xdeaddead' ],
+      dataId: 'u2',
+      cid: 'Qmrandomhash'
+    }],
+  ])
+
+  const mockProxy = { address: '0xdeaddead' }
+  const mockWrapper = { appMetadata: getAppMetadataObservable }
+
+  getAppMetadata(null, mockProxy, mockWrapper).subscribe(value => {
+    t.deepEqual(value, {
+      event: 'AppMetadata',
+      returnValues: [{
+        from: '0xfed',
+        to: ['0xcafe','0xdeaddead' ],
+        dataId: 'u2',
+        cid: 'Qmrandomhash'  
+      }]
+    })
+  })
+})
---
+++ packages/aragon-wrapper/src/rpc/handlers/register-app-metadata.js
@@ -0,0 +1,4 @@
+export default function (request, proxy, wrapper) {
+  console.log('--', request, proxy)
+  return 'reg test'
+}
