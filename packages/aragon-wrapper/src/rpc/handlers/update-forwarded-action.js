c02f9e079ef257444a37f49dfa760728652b744d
Kevin Siegler <kevinsiegler54@gmail.com>
2019-06-25 14:20:06 -0700

    Deconstruct params passed to updateForwardedAction

--- packages/aragon-wrapper/src/rpc/handlers/update-forwarded-action.js
+++ packages/aragon-wrapper/src/rpc/handlers/update-forwarded-action.js
@@ -1,7 +1,15 @@
 export default function (request, proxy, wrapper) {
+  const [
+    actionId,
+    evmScript,
+    state
+  ] = request.params
+  
   wrapper.setForwardedAction(
     proxy.address,
-    request.params
+    actionId,
+    evmScript,
+    state
   )
   return Promise.resolve()
 }
