4df35c2b26715343b11b14add814e9e3e9bc928e
Brett Sun <qisheng.brett.sun@gmail.com>
2019-09-01 16:03:07 +0200

    Wrapper: update get-apps handler
    Adds the ability to filter for just the current context's connected app
    and a 'get' operation to avoid subscribing.

--- packages/aragon-wrapper/src/rpc/handlers/get-apps.js
+++ packages/aragon-wrapper/src/rpc/handlers/get-apps.js
@@ -1,3 +1,31 @@
+import { first, map } from 'rxjs/operators'
+import { addressesEqual } from '../../utils'
+
 export default function (request, proxy, wrapper) {
-  return wrapper.apps
+  const operation = request.params[0]
+  let appCategory = request.params[1]
+  if (appCategory !== 'all' && appCategory !== 'current') {
+    appCategory = 'all'
+  }
+
+  // Backwards compatibility with initial RPC API (no parameters passed)
+  if (operation === undefined) {
+    return wrapper.apps
+  }
+
+  const app$ = appCategory === 'current'
+    ? wrapper.apps.pipe(
+      map(apps => apps.find(app => addressesEqual(app.proxyAddress, proxy.address)))
+    )
+    : wrapper.apps
+  if (operation === 'observe') {
+    return app$
+  }
+  if (operation === 'get') {
+    return app$.pipe(first())
+  }
+
+  return Promise.reject(
+    new Error('Invalid get apps operation')
+  )
 }
--- packages/aragon-wrapper/src/rpc/handlers/get-apps.test.js
+++ packages/aragon-wrapper/src/rpc/handlers/get-apps.test.js
@@ -8,7 +8,7 @@ test.afterEach.always(() => {
   sinon.restore()
 })
 
-test('should return an observable from the app list', async (t) => {
+test('should return a subscription for the entire app list if observing all', async (t) => {
   t.plan(2)
 
   // arrange
@@ -26,17 +26,213 @@ test('should return an observable from the app list', async (t) => {
     abi: 'abi for votingApp',
     proxyAddress: '0x789'
   })
+  const appsMock = of(initialApps, endApps)
 
-  const appsObservable = of(initialApps, endApps)
+  const requestStub = {
+    params: ['observe', 'all']
+  }
+  const proxyStub = {}
+  const wrapperStub = {
+    apps: appsMock
+  }
+
   // act
-  const result = await getApps(null, null, { apps: appsObservable })
+  const result = await getApps(requestStub, proxyStub, wrapperStub)
   // assert
-  let emitIndex = 1
+  let emitIndex = 0
   result.subscribe(value => {
-    if (emitIndex === 1) {
+    if (emitIndex === 0) {
       t.deepEqual(value, initialApps)
-    } else if (emitIndex === 2) {
+    } else if (emitIndex === 1) {
       t.deepEqual(value, endApps)
+    } else {
+      t.fail('too many emissions')
+    }
+
+    emitIndex++
+  })
+})
+
+test('should return a subscription for the entire app list via initial RPC API', async (t) => {
+  t.plan(2)
+
+  // arrange
+  const initialApps = [
+    {
+      appId: 'coolApp',
+      kernelAddress: '0x123',
+      abi: 'abi for coolApp',
+      proxyAddress: '0x456'
+    }
+  ]
+  const endApps = [].concat(initialApps, {
+    appId: 'votingApp',
+    kernelAddress: '0x456',
+    abi: 'abi for votingApp',
+    proxyAddress: '0x789'
+  })
+  const appsMock = of(initialApps, endApps)
+
+  const requestStub = {
+    params: []
+  }
+  const proxyStub = {}
+  const wrapperStub = {
+    apps: appsMock
+  }
+
+  // act
+  const result = await getApps(requestStub, proxyStub, wrapperStub)
+  // assert
+  let emitIndex = 0
+  result.subscribe(value => {
+    if (emitIndex === 0) {
+      t.deepEqual(value, initialApps)
+    } else if (emitIndex === 1) {
+      t.deepEqual(value, endApps)
+    } else {
+      t.fail('too many emissions')
+    }
+
+    emitIndex++
+  })
+})
+
+test('should return a subscription for just the current app if observing current', async (t) => {
+  t.plan(2)
+
+  // arrange
+  const currentAppAddress = '0x456'
+  const initialApp = {
+    appId: 'coolApp',
+    kernelAddress: '0x123',
+    abi: 'abi for coolApp',
+    proxyAddress: currentAppAddress
+  }
+  const endApp = {
+    ...initialApp,
+    appId: 'coolApp',
+  }
+  const appsMock = of(
+    [initialApp],
+    [
+      // This extra app should be filtered out
+      {
+        appId: 'votingApp',
+        kernelAddress: '0x456',
+        abi: 'abi for votingApp',
+        proxyAddress: '0x789'
+      },
+      endApp
+    ]
+  )
+
+  const requestStub = {
+    params: ['observe', 'current']
+  }
+  const proxyStub = {
+    address: currentAppAddress
+  }
+  const wrapperStub = {
+    apps: appsMock
+  }
+
+  // act
+  const result = await getApps(requestStub, proxyStub, wrapperStub)
+  // assert
+  let emitIndex = 0
+  result.subscribe(value => {
+    if (emitIndex === 0) {
+      t.deepEqual(value, initialApp)
+    } else if (emitIndex === 1) {
+      t.deepEqual(value, endApp)
+    } else {
+      t.fail('too many emissions')
+    }
+
+    emitIndex++
+  })
+})
+
+test('should return the initial value for the entire app list if getting all', async (t) => {
+  t.plan(1)
+
+  // arrange
+  const initialApps = [
+    {
+      appId: 'coolApp',
+      kernelAddress: '0x123',
+      abi: 'abi for coolApp',
+      proxyAddress: '0x456'
+    }
+  ]
+  const endApps = [].concat(initialApps, {
+    appId: 'votingApp',
+    kernelAddress: '0x456',
+    abi: 'abi for votingApp',
+    proxyAddress: '0x789'
+  })
+  const appsMock = of(initialApps, endApps)
+
+  const requestStub = {
+    params: ['get', 'all']
+  }
+  const proxyStub = {}
+  const wrapperStub = {
+    apps: appsMock
+  }
+
+  // act
+  const result = await getApps(requestStub, proxyStub, wrapperStub)
+  // assert
+  let emitIndex = 0
+  result.subscribe(value => {
+    if (emitIndex === 0) {
+      t.deepEqual(value, initialApps)
+    } else {
+      t.fail('too many emissions')
+    }
+
+    emitIndex++
+  })
+})
+
+test('should return the initial value for just the current app if getting current', async (t) => {
+  t.plan(1)
+
+  // arrange
+  const currentAppAddress = '0x456'
+  const initialApp = {
+    appId: 'coolApp',
+    kernelAddress: '0x123',
+    abi: 'abi for coolApp',
+    proxyAddress: currentAppAddress
+  }
+  const endApp = {
+    appId: 'coolApp',
+    kernelAddress: '0x123',
+    abi: 'new abi for coolApp',
+    proxyAddress: currentAppAddress
+  }
+  const appsMock = of([initialApp], [endApp])
+
+  const requestStub = {
+    params: ['get', 'current']
+  }
+  const proxyStub = {
+    address: currentAppAddress
+  }
+  const wrapperStub = {
+    apps: appsMock
+  }
+
+  // act
+  const result = await getApps(requestStub, proxyStub, wrapperStub)
+  // assert
+  let emitIndex = 0
+  result.subscribe(value => {
+    if (emitIndex === 0) {
+      t.deepEqual(value, initialApp)
     } else {
       t.fail('too many emissions')
     }
