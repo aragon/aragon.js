23366dffb394cefc098b8ed2ee283fd27ee8c20e
Alexander Wormbs <alexander.wormbs@consensys.net>
2020-02-18 19:27:56 +0000

    add test for signature collisions

--- packages/aragon-wrapper/src/utils/index.test.js
+++ packages/aragon-wrapper/src/utils/index.test.js
@@ -1,10 +1,28 @@
 import test from 'ava'
 import sinon from 'sinon'
 import * as utils from './index'
+import Web3 from 'web3'
 
 test.afterEach.always(() => {
   sinon.restore()
 })
+
+const mockAbi = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "test",
+        "type": "uint256"
+      }
+    ],
+    "name": "testFunction",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
 
 test('should enhance an object to lookup eth addresses easier', async (t) => {
   // arrange
@@ -50,4 +68,33 @@ test('should allow the proxy to be initialized with an object containing any cas
   t.is(permissions['0x0000000000000000000000000000000000000EED'], rainiPermissions)
   t.is(permissions['0x0000000000000000000000000000000000000eed'], rainiPermissions)
   t.is(permissions['0x0000000000000000000000000000000000000EeD'], rainiPermissions)
+})
+
+test('should combine the app ABI with the events and methods from ProxyApp ABI', t => {
+  const contractProxy = utils.makeProxyFromAppABI(null, mockAbi, new Web3(), {})
+
+  t.assert(contractProxy.contract.methods.testFunction)
+  t.assert(contractProxy.contract.methods.proxyType)
+  t.assert(contractProxy.contract.events.ProxyDeposit)
+})
+
+test('should find collisions', t => {
+  const mockAbi2 = [{
+    ...mockAbi[0],
+    "inputs": [
+      {
+        "name": "test2",
+        "type": "uint256"
+      }
+    ],
+  }]
+
+  const collisions = utils.findFunctionSignatureCollisions(mockAbi, mockAbi2)
+
+  t.assert(collisions.length > 0)
+  t.assert(collisions[0].name === mockAbi[0].name)
+  t.is(`WARNING: Collisions detected between the proxy and app contract ABI's.
+       This is a potential security risk.
+       Affected functions:
+       ${collisions}`, '')
 })
